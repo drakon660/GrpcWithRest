647bdc121a1c3f209b29ea7fca01f00dd622c302
drakon <drakon660@gmail.com>
2023-04-21 14:54:02 +0200

    upload

---
+++ GrpcWithRest/GrpcWithRest/Controllers/TestControllers.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GrpcWithRest.Controllers;
+
+[ApiController]
+[Route("test")]
+public class TestControllers : ControllerBase 
+{
+    [Route("test-me")]
+    public IActionResult TestMe()
+    {
+        return Ok("test-me");
+    }
+}
--- GrpcWithRest/GrpcWithRest/Program.cs
+++ GrpcWithRest/GrpcWithRest/Program.cs
@@ -6,12 +6,23 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+
+IWebHostEnvironment env = app.Environment;
+if (env.IsDevelopment())
+{
+    app.MapGrpcReflectionService(); 
+}
+
+app.MapControllers();
+
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
